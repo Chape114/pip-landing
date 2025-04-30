8e7f70b634ca0a2e61b3a011e1cedfbda99916ba
Santi Chiappero <santiachiappero@gmail.com>
2025-04-30 09:15:51 -0300

    🌍 Setea idioma a español y agrega favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,16 @@ import { ThemeProvider } from '../components/theme-provider'
 export const metadata: Metadata = {
   title: 'PiP Studio',
   description: 'Sitios web con diseño, impacto y propósito.',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="es" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
